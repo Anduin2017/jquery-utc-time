b0bef4613f6c160c836b4eb8ef36416002be4792
Anduin Xue <anduin@aiursoft.com>
2020-05-17 15:04:37 +0800

    Upgrade version.

--- src/jquery-utc-time.js
+++ src/jquery-utc-time.js
@@ -16,8 +16,8 @@ class UtcTime {
         };
         Object.assign(defaultSettings, settings);
         this.initFormat();
-        var self = this;
-        var loop = function () {
+        let self = this;
+        let loop = function () {
             self.initTime(defaultSettings, self);
             if (!defaultSettings.disableAutoUpdate) {
                 setTimeout(loop, 1000);
@@ -27,8 +27,8 @@ class UtcTime {
     }
 
     timeSince(date, settings) {
-        var seconds = Math.floor((new Date() - date) / 1000);
-        var interval = Math.floor(seconds / 2592000);
+        let seconds = Math.floor((new Date() - date) / 1000);
+        let interval = Math.floor(seconds / 2592000);
         if (interval > 1) {
             if (settings.format) {
                 return date.Format(settings.format);
@@ -57,24 +57,34 @@ class UtcTime {
         }
     };
 
+    getDate(inputString) {
+        if (inputString.endsWith(' UTC') || inputString.endsWith('Z')) {
+            return new Date(inputString);
+        }
+        if (!inputString.endsWith(' UTC')) {
+            let timevalue = inputString + ' UTC';
+            let date = new Date(timevalue);
+            if (!isNaN(date.getTime())) {
+                return date;
+            }
+        }
+        if (!inputString.endsWith('Z')) {
+            let timevalue = inputString + 'Z';
+            let date = new Date(timevalue);
+            if (!isNaN(date.getTime())) {
+                return date;
+            }
+        }
+        console.warn(`jquery-utc-time: Input string ${inputString} can not be converted to UTC time and try to parse as typical date string.`);
+        return new Date(inputString);
+    }
+
     initTime(settings, self) {
         $('*[' + settings.attr + ']').each(function (t) {
-            var timefield = $(this);
-            var sourcevalue = timefield.attr(settings.attr);
-            var timevalue = sourcevalue;
-            if (!sourcevalue.endsWith(' UTC')) {
-                timevalue = sourcevalue + ' UTC';
-            }
-            var date = new Date(timevalue);
-            if (isNaN(date.getTime())) {
-                if (!sourcevalue.endsWith('Z')) {
-                    timevalue = sourcevalue + 'Z';
-                } else {
-                    timevalue = sourcevalue;
-                }
-                var date = new Date(timevalue);
-            }
-            var text = self.timeSince(date, settings);
+            let timefield = $(this);
+            let sourcevalue = timefield.attr(settings.attr);
+            let date = self.getDate(sourcevalue);
+            let text = self.timeSince(date, settings);
             if (settings.disableAgo) {
                 if (settings.format) {
                     text = date.Format(settings.format);
@@ -94,7 +104,7 @@ class UtcTime {
 
     initFormat() {
         Date.prototype.Format = function (fmt) {
-            var o = {
+            const o = {
                 "M+": this.getMonth() + 1,
                 "d+": this.getDate(),
                 "h+": this.getHours(),
@@ -105,7 +115,7 @@ class UtcTime {
             };
             if (/(y+)/.test(fmt))
                 fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-            for (var k in o)
+            for (let k in o)
                 if (new RegExp("(" + k + ")").test(fmt))
                     fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
             return fmt;
