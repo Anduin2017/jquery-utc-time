451194a4c58c6d240bf88ddfc2110f914318623f
Anduin Xue <anduin@aiursoft.com>
2020-05-18 13:42:06 +0800

    Use tooltip.

--- src/jquery-utc-time.js
+++ src/jquery-utc-time.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import 'bootstrap/js/src/tooltip';
 
 class UtcTime {
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = [
   {
     entry: './src/jquery-utc-time.js',
-    externals: { jquery: 'jQuery' },
+    externals: { jquery: 'jQuery', bootstrap: 'bootstrap', popper: 'popper' },
     mode: "production",
     output: {
       filename: 'jquery-utc-time.min.js',
